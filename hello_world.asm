5df8186338dcdef4097e1051d1058db0483ce80b
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-11-03 22:42:11 -0300

    initial commit

---
+++ hello_world.asm
@@ -0,0 +1,19 @@
+; hello_world.asm
+
+global _start
+
+section .text: 
+_start:
+	mov eax, 0x4		; use the write syscall	
+	mov ebx, 1 		; use stdout as the fd
+	mov ecx, message	; use the message as the buffer 
+	mov edx, message_length ; and supply the lentth
+	int 0x80		; invoke the syscall
+
+	mov eax, 0x1
+	mov ebx, 0		; sucess exit
+	int 0x80	
+
+section .data:
+	message: db "Hello World!", 0xA ; 0xA works like \n
+	message_length: equ $-message
